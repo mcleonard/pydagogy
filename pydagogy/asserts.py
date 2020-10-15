23eecd0a32b0be09c4c887ecc69eef381e5695ae
Mathew Leonard <leonard.mat@gmail.com>
2020-10-15 10:00:42 -0700

    New asserts and type annotations

--- pydagogy/asserts.py
+++ pydagogy/asserts.py
@@ -1,8 +1,16 @@
+from typing import Any, Union, Text
 from functools import wraps
 from .utils import text_format
 
+Numeric = Union[int, float]
+
 __all__ = ["assert_equal", "assert_not_equal", "assert_close"]
 
+
+# Assert functions are convenient methods for comparing objects and returning messages
+# for failed comparisons. All assert functions must include a keyword argument 'message'
+# and return a boolean. Wrap your assert function with the @assert_func decorator to
+# get automated printing of the fail message.
 
 def assert_func(func):
 
@@ -40,24 +48,34 @@ def assert_func(func):
 
 
 @assert_func
-def assert_equal(val1, val2, message=None):
-    """ Test docstring """
+def assert_equal(val1: Any, val2: Any, message: Text = None) -> bool:
+    """ Assert that val1 and val2 are equal. This is valid for any two objects
+        that implement equality comparisons.
+    """
     if val1 != val2:
         return False
     return True
 
 
 @assert_func
-def assert_not_equal(val1, val2, message=None):
+def assert_not_equal(val1: Any, val2: Any, message: Text=None) -> bool:
     if val1 == val2:
         return False
-
     return True
 
 
 @assert_func
-def assert_close(val1, val2, tol=1e-3, message=None):
+def assert_close(val1: Numeric, val2: Numeric, tol: Numeric=1e-3, message: Text=None) -> bool:
     if abs(val1 - val2) > tol:
         return False
-
     return True
+
+
+@assert_func
+def assert_true(condition: bool, message: Text=None) -> bool:
+    return condition
+
+
+@assert_func
+def assert_false(condition: bool, message: Text=None) -> bool:
+    return not condition
